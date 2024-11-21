c07586cc7fc055abd54127f21f36ec269313c6ba
Matiiu <mateotaveraramirez@gmail.com>
2024-11-21 12:19:47 -0500

    feat: Rename product type to DraftProduct and update schema import for draft validation

--- src/types/products/index.ts
+++ src/types/products/index.ts
@@ -1,9 +1,9 @@
 import { z } from 'zod';
 import { type Product as TPrismaProduct, type Category } from '@prisma/client';
-import { productSchema } from '@/src/schemas/products';
+import { draftProductSchema } from '@/src/schemas/products';
 
 export type ProductWithCategory = TPrismaProduct & {
 	category: Category;
 };
 
-export type Product = z.infer<typeof productSchema>;
+export type DraftProduct = z.infer<typeof draftProductSchema>;
