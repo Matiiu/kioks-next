2929a0d52d02f83b1427d9c5bda6264c26dc05c2
Matiiu <mateotaveraramirez@gmail.com>
2024-11-23 12:25:02 -0500

    fix: Change NewProductPage to a synchronous function for improved rendering

--- app/admin/products/new/page.tsx
+++ app/admin/products/new/page.tsx
@@ -3,7 +3,7 @@ import AddProductForm from '@/components/products/AddProductForm';
 import ProductInputs from '@/components/products/ProductInputs';
 import GoBackButton from '@/components/ui/GoBackButton';
 
-export default async function NewProductPage() {
+export default function NewProductPage() {
 	return (
 		<>
 			<Heading>Nuevo Productos</Heading>
